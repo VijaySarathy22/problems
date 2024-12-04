8b7514b9e11ae7ac882e9ac1d853f03b103ca654
vijayasarathy <159127834+VijaySarathy22@users.noreply.github.com>
2024-12-04 10:15:18 +0530

    Add files via upload

---
+++ xy.java
@@ -0,0 +1,18 @@
+class Main {
+    public static void main(String[] args) {
+        int x=2;
+        int y=3;
+        for( y=3;y>=1;y--){
+        System.out.println(x);
+        }
+    }
+}
+/*Write a program which takes two values x and y. Prints x for y number of times.
+Input:
+2 
+3
+Expected Output
+2
+2
+2
+Explanation - 2 is x and 3 is y in the input. So 2 is printed 3 times on the output.*/
