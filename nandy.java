0ee2654efad353cd233b059256007c98a24bbfea
vijayasarathy <159127834+VijaySarathy22@users.noreply.github.com>
2024-12-04 10:16:12 +0530

    Add files via upload

---
+++ nandy.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        String firstname="nandy";
+        String lastname= "raja";
+        String name=(firstname + lastname);
+        for( int n=5;n>=1;n--){
+        System.out.println(name);
+        }
+    }
+}
